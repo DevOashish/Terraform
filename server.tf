734c7ec6ae75310415ccda0aa43ca57ee5fa4afd
DevOashish <ashich870@gmail.com>
2023-05-18 22:02:08 +0530

    latest chnages

--- server.tf
+++ server.tf
@@ -3,15 +3,19 @@ data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
+
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
+
 variable "instance_type" {
   default = "t3.small"
 }
+
 variable "components" {
   default = [ "frontend", "mongodb", "catalogue" ]
 }
+
 resource "aws_instance" "instance" {
   count         = length(var.components)
   ami           = data.aws_ami.centos.image_id
@@ -19,7 +23,6 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = "catalogue"
     Name = var.components[count.index]
   }
 }
