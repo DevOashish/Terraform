e2d6e51b798f872e7b27d7593caade2b5da05ef2
DevOashish <ashich870@gmail.com>
2023-05-26 11:23:55 +0530

    comment

--- server.tf
+++ server.tf
@@ -1,24 +1,19 @@
-data "aws_ami" "centos" {
-  owners           = ["973714476881"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-}
+# resource "aws_instance" "instance" {
+#   for_each         = var.components
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = each.value["instance_type"]
+#   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
+#   tags = {
+#     Name = each.value["name"]
+#   }
+# }
 
-variable "components" {
-  default = [ "frontend", "mongodb", "catalogue" ]
-}
-
-resource "aws_instance" "instance" {
-  count         = length(var.components)
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = var.components[count.index]
-  }
-}
+# resource "aws_route53_record" "records" {
+#   for_each = var.components
+#   zone_id = "Z006270827E18HEX0RQPW"
+#   name = "${each.value["name"]}-dev.devoash.tech"
+#   type = "A"
+#   tt1 = 30
+#   records = [aws_instance.instance[each.value["name"]].private_ip]
+#   }
