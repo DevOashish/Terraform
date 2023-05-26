58a36e6783a64ce30346b98d3f56d0f3e698a0f2
DevOashish <ashich870@gmail.com>
2023-05-26 13:23:24 +0530

    latest

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,8 @@
-resource "aws_instance" "web"{
+resource "aws_vpc" "main" { #this name belongs to only terraform reference
 
-    ami = "ami-0bef6cc322bfff646" #AMI ID will be different every region, this is OS
-    instance_type = "t3.micro"
-    
+    cidr_block       = "10.0.0.0/16"
+    instance_tenancy = "default"
+    tags = {
+        Name = "automated-vpc" #this name belongs to AWS
+    }
 }
